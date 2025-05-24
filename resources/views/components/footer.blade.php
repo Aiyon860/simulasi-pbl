84f6ccd591e5c1fb67246126f061d79aaa9137d9
danielaiyon <yonyondap23@gmail.com>
2025-05-24 22:22:30 +0700

    hapus livewire-tables dan views

--- resources/views/components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="mt-4 text-center text-sm text-white/60 border-t border-white/10 pt-2">
-    <div class="container mx-auto">
-        {{ date('Y') }} &copy; Polines
-    </div>
-</footer>

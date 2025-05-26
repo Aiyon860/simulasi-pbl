8be2af8efdbe1da403956ed858049f48a09818f6
danielaiyon <yonyondap23@gmail.com>
2025-05-27 00:28:15 +0700

    menperbaiki dokumentasi API untuk endpoint Auth dan Barang

---
+++ app/Http/Resources/KategoriBarangResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class KategoriBarangResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nama_kategori_barang' => $this->nama_kategori_barang,
+        ];
+    }
+}
